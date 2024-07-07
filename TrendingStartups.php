60ee1fdbd6e66493640418dc1b09daefc1808b8e
Angel Cee <39429664+JossyMall@users.noreply.github.com>
2024-07-07 06:10:13 -0700

    Update TrendingStartups.php

--- TrendingStartups.php
+++ TrendingStartups.php
@@ -1,1 +1,23 @@
+<?php
+
+namespace Piwik\Plugins\MatomoCommunity;
+
+use Piwik\Db;
+
+class TrendingStartups
+{
+    public function getTrendingStartups()
+    {
+        $db = Db::get();
+        return $db->fetchAll('SELECT site_id, name, growth_rate FROM matomo_trending_startups ORDER BY growth_rate DESC LIMIT 10');
+    }
+
+    public function displayTrendingStartups()
+    {
+        $startups = $this->getTrendingStartups();
+        foreach ($startups as $startup) {
+            echo "Startup: " . htmlspecialchars($startup['name']) . " - Growth Rate: " . htmlspecialchars($startup['growth_rate']) . "%<br>";
+        }
+    }
+}
 
