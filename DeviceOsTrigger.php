ccdf1eb8e215f1e2177981c990cdcb087638eb10
Angel Cee <39429664+JossyMall@users.noreply.github.com>
2024-07-07 06:06:24 -0700

    Update DeviceOsTrigger.php

--- DeviceOsTrigger.php
+++ DeviceOsTrigger.php
@@ -1,1 +1,28 @@
+<?php
+
+namespace Piwik\Plugins\MatomoCommunity;
+
+use Piwik\Plugin;
+use Piwik\Tracker\Visit;
+
+class DeviceOsTrigger extends Plugin
+{
+    public function registerEvents()
+    {
+        return [
+            'Tracker.newVisitorInformation' => 'addDeviceOsTrigger'
+        ];
+    }
+
+    public function addDeviceOsTrigger(&$visitorInfo)
+    {
+        $visitorInfo['device_os'] = $this->getDeviceOs();
+    }
+
+    private function getDeviceOs()
+    {
+        $os = Visit::getDeviceType();
+        return $os;
+    }
+}
 
