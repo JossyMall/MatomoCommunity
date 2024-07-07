cda8833bd7838dcf76c8791511ca2bd0c393f68b
Angel Cee <39429664+JossyMall@users.noreply.github.com>
2024-07-07 06:44:41 -0700

    Update MembershipFeature.php

--- MembershipFeature.php
+++ MembershipFeature.php
@@ -1,59 +1,57 @@
-<?php
-
 namespace Piwik\Plugins\MatomoCommunity;
 
 use Piwik\Db;
-use Piwik\Piwik;
 
 class MembershipFeature
 {
-    public function createGroup($groupName, $features)
+    public function createMembershipGroup($name, $features)
     {
         $db = Db::get();
         $db->insert('matomo_membership_groups', [
-            'group_name' => $groupName,
+            'name' => $name,
             'features' => json_encode($features)
         ]);
     }
 
-    public function assignUserToGroup($userId, $groupId)
+    public function addUserToGroup($userId, $groupId)
     {
         $db = Db::get();
-        $db->update('matomo_user', ['group_id' => $groupId], 'login = ?', [$userId]);
+        $db->update('matomo_user', ['group_id' => $groupId], 'user_id = ?', [$userId]);
     }
 
     public function setDefaultGroup($groupId)
     {
         $db = Db::get();
-        $db->update('matomo_membership_groups', ['is_default' => 0]);
-        $db->update('matomo_membership_groups', ['is_default' => 1], 'id = ?', [$groupId]);
+        $db->update('matomo_option', ['option_value' => $groupId], 'option_name = ?', ['default_membership_group']);
     }
 
-    public function getUserGroup($userId)
+    public function getMembershipGroups()
     {
         $db = Db::get();
-        return $db->fetchOne('SELECT group_id FROM matomo_user WHERE login = ?', [$userId]);
+        return $db->fetchAll('SELECT * FROM matomo_membership_groups');
     }
 
-    public function getGroupFeatures($groupId)
+    public function getFeaturesForGroup($groupId)
     {
         $db = Db::get();
         $features = $db->fetchOne('SELECT features FROM matomo_membership_groups WHERE id = ?', [$groupId]);
         return json_decode($features, true);
     }
 
-    public function getDefaultGroup()
+    public function isFeatureAvailable($userId, $feature)
     {
         $db = Db::get();
-        return $db->fetchOne('SELECT id FROM matomo_membership_groups WHERE is_default = 1');
+        $groupId = $db->fetchOne('SELECT group_id FROM matomo_user WHERE user_id = ?', [$userId]);
+        $features = $this->getFeaturesForGroup($groupId);
+        return in_array($feature, $features);
     }
 
-    public function displayGroupFeatures($userId)
+    public function displayMembershipPlans()
     {
-        $groupId = $this->getUserGroup($userId);
-        $features = $this->getGroupFeatures($groupId);
-
-        echo "Features available: " . implode(', ', $features);
+        $groups = $this->getMembershipGroups();
+        foreach ($groups as $group) {
+            echo "Group: " . htmlspecialchars($group['name']) . "<br>";
+            echo "Features: " . htmlspecialchars(implode(', ', json_decode($group['features'], true))) . "<br><br>";
+        }
     }
 }
-
