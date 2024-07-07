9f6ea13ffff8cad159be5c131fb6de738ba0ddd2
Angel Cee <39429664+JossyMall@users.noreply.github.com>
2024-07-07 06:08:48 -0700

    Update GrowthWorthGraph.php

--- GrowthWorthGraph.php
+++ GrowthWorthGraph.php
@@ -1,1 +1,27 @@
+<?php
+
+namespace Piwik\Plugins\MatomoCommunity;
+
+use Piwik\View;
+use Piwik\API\Request;
+
+class GrowthWorthGraph
+{
+    public function renderGrowthGraph($siteId, $months = 4)
+    {
+        $endDate = date("Y-m-d");
+        $startDate = date("Y-m-d", strtotime("-$months months"));
+
+        $view = new View('@MatomoCommunity/growth_worth_graph.twig');
+        $view->growthData = $this->getGrowthData($siteId, $startDate, $endDate);
+        return $view->render();
+    }
+
+    private function getGrowthData($siteId, $startDate, $endDate)
+    {
+        $request = new Request("method=VisitsSummary.get&format=JSON&idSite=$siteId&period=day&date=$startDate,$endDate");
+        $result = $request->process();
+        return $result;
+    }
+}
 
