e8b16a34e0c21e269b7ec2a077b778a875a5838d
Angel Cee <39429664+JossyMall@users.noreply.github.com>
2024-07-07 06:07:46 -0700

    Update ValueWorth.php

--- ValueWorth.php
+++ ValueWorth.php
@@ -1,1 +1,25 @@
+<?php
+
+namespace Piwik\Plugins\MatomoCommunity;
+
+use Piwik\Db;
+
+class ValueWorth
+{
+    public function calculateValue($siteId)
+    {
+        $db = Db::get();
+        $uniqueVisitors = $db->fetchOne('SELECT COUNT(DISTINCT idvisitor) FROM matomo_log_visit WHERE idsite = ?', [$siteId]);
+        $actions = $db->fetchOne('SELECT COUNT(*) FROM matomo_log_link_visit_action WHERE idsite = ?', [$siteId]);
+
+        $value = $uniqueVisitors + ($actions * 0.001);
+        return $value;
+    }
+
+    public function displayValue($siteId)
+    {
+        $value = $this->calculateValue($siteId);
+        echo "Value: $" . number_format($value, 2);
+    }
+}
 
