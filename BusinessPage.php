3ffeb2ec47d883a6a22fd130b5ecf130ae6b0009
Angel Cee <39429664+JossyMall@users.noreply.github.com>
2024-07-07 06:08:23 -0700

    Update BusinessPage.php

--- BusinessPage.php
+++ BusinessPage.php
@@ -1,1 +1,27 @@
+<?php
+
+namespace Piwik\Plugins\MatomoCommunity;
+
+use Piwik\Db;
+
+class BusinessPage
+{
+    public function updateUserRole($userId, $role)
+    {
+        $db = Db::get();
+        $db->update('matomo_user', ['role' => $role], 'login = ?', [$userId]);
+    }
+
+    public function getUserRole($userId)
+    {
+        $db = Db::get();
+        return $db->fetchOne('SELECT role FROM matomo_user WHERE login = ?', [$userId]);
+    }
+
+    public function displayUserRole($userId)
+    {
+        $role = $this->getUserRole($userId);
+        echo "Role: " . htmlspecialchars($role);
+    }
+}
 
