8a5048d8274f2d7424d6706305950b2379bfaaeb
Angel Cee <39429664+JossyMall@users.noreply.github.com>
2024-07-07 06:11:10 -0700

    Update MakeOffer.php

--- MakeOffer.php
+++ MakeOffer.php
@@ -1,1 +1,27 @@
+<?php
+
+namespace Piwik\Plugins\MatomoCommunity;
+
+use Piwik\Db;
+
+class MakeOffer
+{
+    public function makeOffer($fromUserId, $toUserId, $siteId, $message)
+    {
+        $db = Db::get();
+        $db->insert('matomo_offers', [
+            'from_user_id' => $fromUserId,
+            'to_user_id' => $toUserId,
+            'site_id' => $siteId,
+            'message' => $message,
+            'timestamp' => time()
+        ]);
+    }
+
+    public function getOffers($userId)
+    {
+        $db = Db::get();
+        return $db->fetchAll('SELECT * FROM matomo_offers WHERE to_user_id = ?', [$userId]);
+    }
+}
 
