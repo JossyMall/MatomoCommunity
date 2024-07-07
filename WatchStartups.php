5ee5ed0685404ed3abe7f13608f2d4472e1f6922
Angel Cee <39429664+JossyMall@users.noreply.github.com>
2024-07-07 06:12:25 -0700

    Update WatchStartups.php

--- WatchStartups.php
+++ WatchStartups.php
@@ -1,1 +1,33 @@
 
+<?php
+
+namespace Piwik\Plugins\MatomoCommunity;
+
+use Piwik\Db;
+
+class WatchStartups
+{
+    public function addToWatchlist($userId, $siteId)
+    {
+        $db = Db::get();
+        $db->insert('matomo_watchlist', [
+            'user_id' => $userId,
+            'site_id' => $siteId
+        ]);
+    }
+
+    public function getWatchlist($userId)
+    {
+        $db = Db::get();
+        return $db->fetchAll('SELECT * FROM matomo_watchlist WHERE user_id = ?', [$userId]);
+    }
+
+    public function displayWatchlist($userId)
+    {
+        $watchlist = $this->getWatchlist($userId);
+        foreach ($watchlist as $item) {
+            echo "Watched Site ID: " . htmlspecialchars($item['site_id']) . "<br>";
+        }
+    }
+}
+
