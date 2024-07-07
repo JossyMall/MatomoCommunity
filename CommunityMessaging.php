4a5773497ad0f0ec55d2a0d84960b31abf515459
Angel Cee <39429664+JossyMall@users.noreply.github.com>
2024-07-07 06:07:01 -0700

    Update CommunityMessaging.php

--- CommunityMessaging.php
+++ CommunityMessaging.php
@@ -1,1 +1,26 @@
+<?php
+
+namespace Piwik\Plugins\MatomoCommunity;
+
+use Piwik\Db;
+
+class CommunityMessaging
+{
+    public function sendMessage($fromUserId, $toUserId, $message)
+    {
+        $db = Db::get();
+        $db->insert('matomo_messages', [
+            'from_user_id' => $fromUserId,
+            'to_user_id' => $toUserId,
+            'message' => $message,
+            'timestamp' => time()
+        ]);
+    }
+
+    public function getMessages($userId)
+    {
+        $db = Db::get();
+        return $db->fetchAll('SELECT * FROM matomo_messages WHERE to_user_id = ?', [$userId]);
+    }
+}
 
